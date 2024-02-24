69e4611ba5b197fbb19d73986516d1448d549655
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-02-24 18:00:06 +0100

    Add dark theme to btns & fix links hover effect

--- src/Pages/AboutMe/ButtonNavigation.jsx
+++ src/Pages/AboutMe/ButtonNavigation.jsx
@@ -1,12 +1,23 @@
+import { useContext } from 'react';
 import { Stack } from 'react-bootstrap';
+import { DarkModeTheme } from '../../context/dark-mode-theme-context';
 
 export default function ButtonNavigation() {
+	const [isDarkMode] = useContext(DarkModeTheme);
 	return (
 		<Stack direction='horizontal' className='justify-content-center' gap={4}>
-			<a className='btn btn-light custom-btn-border' href='#' role='button'>
+			<a
+				className={`btn ${isDarkMode ? 'btn-dark' : 'btn-light'} custom-btn-border`}
+				href='#'
+				role='button'
+			>
 				Contact Me
 			</a>
-			<a className='btn btn-light custom-btn-border' href='#' role='button'>
+			<a
+				className={`btn ${isDarkMode ? 'btn-dark' : 'btn-light'} custom-btn-border`}
+				href='#'
+				role='button'
+			>
 				See My Work
 			</a>
 		</Stack>
