308a5649a037af40e0532dab69903139e86ce8f0
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-02-24 16:07:25 +0100

    implement the theme toggler for the whole page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AboutMe from './Pages/AboutMe/main';
 import { DarkModeTheme } from './context/dark-mode-theme-context';
 
 export default function App() {
 	const [isDarkMode, setIsDarkMode] = useState(true);
+
+	useEffect(() => {
+		/*
+		the data attribute data-bs-theme provided by Bootstrap does not work properly unless I add it to html or body element, & since these are outside of React reach (as far as I know), I had to implement the dynamic change in this way.
+		 */
+		const bodyElement = document.body;
+		bodyElement.dataset.bsTheme = isDarkMode ? 'dark' : 'light';
+	}, [isDarkMode]);
+
 	function updateDarkModeState() {
 		setIsDarkMode((prev) => !prev);
 	}
+
 	return (
 		<DarkModeTheme.Provider value={[isDarkMode, updateDarkModeState]}>
 			<AboutMe />
