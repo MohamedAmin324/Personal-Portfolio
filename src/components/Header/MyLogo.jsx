ed42291058f76f8ba13fa2a041b886ed1489d134
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-02-25 17:04:39 +0100

    Add animated Hamburger menu icon

--- src/components/Header/MyLogo.jsx
+++ src/components/Header/MyLogo.jsx
@@ -1,12 +1,13 @@
 import { Navbar, Stack } from 'react-bootstrap';
 import imgUrl from '../../assets/navigation-bar-icons/Light-Mode/coding-svgrepo-com.svg';
 import imgDarkUrl from '../../assets/navigation-bar-icons/Dark-Mode/coding-svgrepo-com.svg';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { DarkModeTheme } from '../../context/dark-mode-theme-context';
 
 // eslint-disable-next-line react/prop-types
 export default function MyLogo({ isSmallScreen }) {
 	const [isDarkMode] = useContext(DarkModeTheme);
+	const [isClicked, setIsClicked] = useState(false);
 	return (
 		<Stack
 			direction='horizontal'
@@ -14,7 +15,21 @@ export default function MyLogo({ isSmallScreen }) {
 		>
 			<img src={isDarkMode ? imgDarkUrl : imgUrl} />
 			<div className={isSmallScreen ? 'ms-1' : 'ms-1 fw-bolder'}>Med Amine</div>
-			<Navbar.Toggle aria-controls='navbar-nav' />
+			{isSmallScreen ? (
+				<Navbar.Toggle
+					bsPrefix='hamburger-menu'
+					aria-controls='navbar-nav'
+					as='div'
+					onClick={() => setIsClicked(!isClicked)}
+				>
+					<input
+						type='checkbox'
+						checked={isClicked}
+						onChange={() => setIsClicked(!isClicked)}
+						className={`customized-checkbox ${isClicked ? 'test' : ''}`}
+					/>
+				</Navbar.Toggle>
+			) : null}
 		</Stack>
 	);
 }
