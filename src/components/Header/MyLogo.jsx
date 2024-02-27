8a57ef1b6bddb6016fdef9d4151475aaf3f36efe
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-02-27 17:13:09 +0100

    Refactor MyLogo component
    Make the logo a link that returns to the Home page which is the AboutMe page

--- src/components/Header/MyLogo.jsx
+++ src/components/Header/MyLogo.jsx
@@ -3,6 +3,7 @@ import imgUrl from '../../assets/navigation-bar-icons/Light-Mode/coding-svgrepo-
 import imgDarkUrl from '../../assets/navigation-bar-icons/Dark-Mode/coding-svgrepo-com.svg';
 import { useContext, useState } from 'react';
 import { DarkModeTheme } from '../../context/dark-mode-theme-context';
+import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 export default function MyLogo({ isSmallScreen }) {
@@ -13,8 +14,19 @@ export default function MyLogo({ isSmallScreen }) {
 			direction='horizontal'
 			className='justify-content-between justify-content-md-start'
 		>
-			<img src={isDarkMode ? imgDarkUrl : imgUrl} />
-			<div className={isSmallScreen ? 'ms-1' : 'ms-1 fw-bolder'}>Med Amine</div>
+			<Link to='/'>
+				<img src={isDarkMode ? imgDarkUrl : imgUrl} />
+				{isSmallScreen ? null : (
+					<div className={isSmallScreen ? 'ms-1' : 'ms-1 fw-bolder'}>
+						Med Amine
+					</div>
+				)}
+			</Link>
+			{isSmallScreen ? (
+				<div className={isSmallScreen ? 'ms-1' : 'ms-1 fw-bolder'}>
+					Med Amine
+				</div>
+			) : null}
 			{isSmallScreen ? (
 				<Navbar.Toggle
 					bsPrefix='hamburger-menu'
