fbb9d35963657937ad01a07380a0468f9772678f
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-03-01 15:55:01 +0100

    Add Email functionality

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,27 +1,51 @@
 import { Button, Form } from 'react-bootstrap';
 import HeroSectionTemplate from '../../components/HeroSectionTemplate';
+import emailjs from '@emailjs/browser';
 import FormInput from './FormInput';
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { DarkModeTheme } from '../../context/dark-mode-theme-context';
 
 export default function ContactHero() {
 	const [isDarkMode] = useContext(DarkModeTheme);
+	const contactForm = useRef(null);
+
+	useEffect(() => {
+		window.onload = () => {
+			emailjs.init({
+				publicKey: 'b3JKOQxCz9TP9J-sj',
+			});
+		};
+		return () => {
+			window.onload = null;
+		};
+	}, []);
+
+	function handleSubmit(eventObj) {
+		eventObj.preventDefault();
+
+		emailjs
+			.sendForm('service_ohvs2p7', 'template_pu7andg', contactForm.current)
+			.then(() => console.log('SUCCESS'))
+			.catch((err) => console.log(err));
+	}
 
 	return (
 		<HeroSectionTemplate mainTitle='Contact Me'>
-			<Form className='contact-form'>
-				<FormInput
-					htmlFor='email-input'
-					id='email-input'
-					type='email'
-					labelText='Email'
-				/>
-
+			<Form ref={contactForm} className='contact-form' onSubmit={handleSubmit}>
 				<FormInput
 					htmlFor='name-input'
 					id='name-input'
+					name='from_name'
 					type='text'
 					labelText='Full Name'
+				/>
+
+				<FormInput
+					htmlFor='email-input'
+					id='email-input'
+					name='user_email'
+					type='email'
+					labelText='Email'
 				/>
 
 				<Form.Group className='mx-auto custom-width'>
@@ -31,6 +55,7 @@ export default function ContactHero() {
 					<Form.Control
 						id='message-body'
 						as='textarea'
+						name='message'
 						className='p-3'
 						style={{ height: '150px' }}
 					/>
--- src/Pages/Contact/FormInput.jsx
+++ src/Pages/Contact/FormInput.jsx
@@ -1,13 +1,13 @@
 import { Form } from 'react-bootstrap';
 
 export default function FormInput(props) {
-	const { htmlFor, type, id, labelText } = props;
+	const { htmlFor, type, id, labelText, name } = props;
 	return (
 		<Form.Group className='mx-auto custom-width'>
 			<Form.Label htmlFor={htmlFor} className='fs-5'>
 				{labelText}
 			</Form.Label>
-			<Form.Control id={id} type={type} />
+			<Form.Control id={id} type={type} name={name} />
 		</Form.Group>
 	);
 }
