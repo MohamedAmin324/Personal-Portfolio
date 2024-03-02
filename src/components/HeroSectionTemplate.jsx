d10f4396219d2f3877f61334cd93626f24bfff56
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-03-02 19:16:57 +0100

    Make document title display page name

--- src/components/HeroSectionTemplate.jsx
+++ src/components/HeroSectionTemplate.jsx
@@ -1,7 +1,10 @@
+import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 
-
 export default function HeroSectionTemplate({ mainTitle, children }) {
+	useEffect(() => {
+		document.title = mainTitle;
+	}, []);
 	return (
 		<Container className='custom-margin-top' fluid='sm'>
 			<h1 className='text-center custom-header fw-bolder'>{mainTitle}</h1>
