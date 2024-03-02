6738645039f9756472bdf3f39c9d083fe4100d80
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-03-02 18:35:52 +0100

    Add Resume page basic content

--- src/Pages/Resume/Resume.jsx
+++ src/Pages/Resume/Resume.jsx
@@ -1,3 +1,10 @@
+import { Button } from 'react-bootstrap';
+import HeroSectionTemplate from '../../components/HeroSectionTemplate';
+
 export default function Resume() {
-	return <h1>This is the Resume Section, (For Testing Purposes)</h1>;
+	return (
+		<HeroSectionTemplate mainTitle='Resume'>
+			<Button className='mx-auto d-block'>Download my CV</Button>
+		</HeroSectionTemplate>
+	);
 }
